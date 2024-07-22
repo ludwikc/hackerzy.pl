35710bab956869ddac1e70faaa8ff486db9cc1df
ludwikc <ludwikc@siadlak.com>
2024-07-22 14:44:14 +0200

    pricefix

--- latest/assets/js/hackerzy-email-to-easycart.js
+++ latest/assets/js/hackerzy-email-to-easycart.js
@@ -4,8 +4,8 @@
                 var email = $('#emailInput').val();
                 
                 // Construct the URL with the email parameter
-                var baseUrl = 'https://app.easycart.pl/checkout/siadlak/hackerzy';
-                var url = baseUrl + '?email=' + encodeURIComponent(email);
+                var baseUrl = 'https://app.easycart.pl/checkout/siadlak/hackerzy?plan=price_1PfLOUILo74O6XObDLEv8Onk&';
+                var url = baseUrl + 'email=' + encodeURIComponent(email);
                 
                 // Redirect to the constructed URL
                 window.location.href = url;
