bd42ce766cb5f8328640feee226fb065d17ae281
ludwikc <ludwikc@siadlak.com>
2024-07-22 16:13:27 +0200

    Cart Link Mobile Fix

--- latest/assets/js/hackerzy-email-to-easycart.js
+++ latest/assets/js/hackerzy-email-to-easycart.js
@@ -1,11 +1,12 @@
  $(document).ready(function() {
             $('#submitButton').on('click', function() {
                 // Capture the email input value
-                var email = $('#emailInput').val();
+                var email = encodeURIComponent($('#emailInput').val());
                 
                 // Construct the URL with the email parameter
-                var baseUrl = 'https://app.easycart.pl/checkout/siadlak/hackerzy?plan=price_1PfLOUILo74O6XObDLEv8Onk&';
-                var url = baseUrl + 'email=' + encodeURIComponent(email);
+                var baseUrl = 'https://app.easycart.pl/checkout/siadlak/hackerzy';
+                var email = encodeURIComponent($('#emailInput').val());
+                var url = baseUrl + '?email=' + email;
                 
                 // Redirect to the constructed URL
                 window.location.href = url;
